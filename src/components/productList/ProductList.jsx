2a1c7de046c7c4ecf74e5da19286b140c0684a88
Huzaifa246 <huzaifa24680@gmail.com>
2024-09-01 16:30:36 +0500

    Update Resume

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -5,7 +5,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@mui/material/Grid";
-import proj1 from "./proj1.jpg";
+import ezra from "../../img/SignUp_PaymentPlan.png";
+import anime from "../../img/anime-banner.JPG";
+import laptop from "../../img/enclair_filtered-products.JPG";
 import proj2 from "./proj2.JPG";
 import proj3 from "../../img/next-js-portfolio.png";
 import Afo from "../../img/afoImg.png";
@@ -20,6 +22,26 @@ const sessionsData = [
     Applink: "https://afomedia.com/login",
   },
   {
+    Name: "Next JS Protfolio",
+    Image: proj3,
+    Applink: "https://next-js-portfolio-henna.vercel.app/",
+  },
+  {
+    Name: "Ezra Managment System",
+    Image: ezra,
+    Applink: "https://ezra-home.kbadosh.com/",
+  },
+  {
+    Name: "Best Anime Site",
+    Image: anime,
+    Applink: "https://next-js-sside.vercel.app/",
+  },
+  {
+    Name: "Laptop Web App",
+    Image: laptop,
+    Applink: "https://enclair.tech/",
+  },
+  {
     Name: "ITS Trading App",
     Image: ITS,
     Applink: "https://trading-its-app.vercel.app/login",
@@ -30,39 +52,28 @@ const sessionsData = [
     Applink: "https://watspi-user.vercel.app/dashboard",
   },
   {
-    Name: "Quiz App in React",
-    Image: proj1,
-    Applink: "https://gleaming-creponne-4938a2.netlify.app/",
-  },
-  {
-    Name: "Front End Project on Html| CSS | JS",
+    Name: "Front End on Html| CSS | JS",
     Image: proj2,
     Applink: "https://coruscating-stroopwafel-464404.netlify.app/",
-  },
-  {
-    Name: "Next JS Protfolio",
-    Image: proj3,
-    Applink: "https://next-js-portfolio-henna.vercel.app/",
   },
 ];
 export default function ProductList() {
   return (
     <>
       <Grid container>
-        {/* {Array.from(Array(12)).map((_, index) => ( */}
         {sessionsData.map((index) => (
           <Grid
             item
-            xs={12} // occupy full width on small screens
-            md={4} // occupy 4 columns on medium and large screens
-            textAlign="center" // align the content to the center
+            xs={12} 
+            md={4}
+            textAlign="center"
             key={index}
-            py={3} // use shorthand prop for vertical padding
-            px={2} // use shorthand prop for horizontal padding
+            py={3}
+            px={2}
           >
             <Card sx={{ maxWidth: 500 }} className="Main_card">
               <CardMedia
-                sx={{ width: "100%", height: "60vh" }} // set the image width to 100% of the card
+                sx={{ width: "100%", height: "60vh", objectFit: "cover" }}
                 component="img"
                 height="120"
                 image={index.Image}
@@ -92,27 +103,10 @@ export default function ProductList() {
         ))}
       </Grid>
       <div style={{ textAlign: "center" }}>
-        <h1
-          style={{
-            fontSize: "36px",
-            color: "#4a4a4a",
-            fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
-            fontWeight: "bold",
-            letterSpacing: "2px",
-            textTransform: "uppercase",
-          }}
-        >
+        <h1 className="coming-soon">
           More Projects Coming Soon!
         </h1>
-        <p
-          style={{
-            fontSize: "20px",
-            color: "#4a4a4a",
-            fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
-            fontWeight: "bold",
-            letterSpacing: "1px",
-          }}
-        >
+        <p className="soon-p">
           Stay tuned for our latest updates and exciting new projects on my Github Account!
         </p>
         <a href="https://github.com/Huzaifa246">
